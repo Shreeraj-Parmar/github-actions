1dba731b59469b3236fe291e54bf32e06e918e48
Shreeraj-Parmar <mrobroie@yahoo.com>
2025-09-20 18:19:19 +0530

    Hot issue Fixed ...

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  let hello;
+  // let hello;
 
   return (
     <div className={styles.page}>
